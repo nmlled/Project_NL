4c7a0d87640f91b6b8d874a96ca50d3251fa64ff
nmlled <Nuno@Nunos-MacBook-Pro.local>
2014-01-14 11:12:39 +0000

    versao4

--- Project_NL/src/com/example/project_nl/MainActivity.java
+++ Project_NL/src/com/example/project_nl/MainActivity.java
@@ -10,23 +10,31 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
-
-
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements LocationListener {
 	
-	private Camera camera;
-	private TextView coordinatesView;
+	//private Camera camera;
+	private TextView viewLatitude;
+	private TextView viewLongitude;
+	private EditText speciesName;
     private LocationManager locationManager;
-    //private boolean validCoordinates = false;
+    private Button speciesGuarda;
+    
+    private Location lastLocation = null;
 
-    private static int clickCount = 0;
+    private void updateUI()
+    {
+		viewLatitude.setText(String.valueOf(lastLocation.getLatitude()));  
+		viewLongitude.setText(String.valueOf(lastLocation.getLongitude())); 
+    }
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -36,76 +44,69 @@ public class MainActivity extends Activity {
 		
 		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		
-		coordinatesView = (TextView) findViewById(R.id.viewCoordinates);
+		viewLatitude = (TextView) findViewById(R.id.viewLatitude);
+		viewLongitude = (TextView) findViewById(R.id.viewLongitude);
+		speciesName = (EditText) findViewById(R.id.editText1);
+		speciesGuarda = (Button) findViewById(R.id.save);
 		
 		// - location button
 	    findViewById(R.id.locationBtn).setOnClickListener(new OnClickListener() {                
 	            @Override
 	            public void onClick(View v) {
-					Log.i("Testes", "Click");
-					coordinatesView.setText("Click " + ++clickCount);
+					Log.i("Testes", "Click Obter");
 	            	// Obtain coordinates
 					
 					//List<String> providers = locationManager.getAllProviders();
 					List<String> providers = locationManager.getProviders(true);
 					
-					
 					for(String provider : providers)
 						Log.i("Testes", provider);
 					
-	            	Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	            	if(loc != null)
-	            		coordinatesView.setText(String.valueOf(loc.getLatitude()));
+					if(lastLocation == null)
+					{
+						lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		            	if(lastLocation != null)
+		            		updateUI();
+					}
 	            }
 	    });
-	    /*
-	    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 60 * 5, 1, 
-	    		new LocationListener() {
-
-					@Override
-					public void onLocationChanged(Location location) 
-					{
-						Log.i("Testes", "Location changed");
-		            	if(location != null)
-		            		coordinatesView.setText(String.valueOf(location.getLatitude()));
-					}
-
-					@Override
-					public void onProviderDisabled(String provider) {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public void onProviderEnabled(String provider) {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public void onStatusChanged(String provider, int status,
-							Bundle extras) {
-						// TODO Auto-generated method stub
-						
-					}});
-		*/
-	    
 	     
-	    // capture button
-        findViewById(R.id.btn_take_photo).setOnClickListener(new OnClickListener() {
-                       @Override
-                       public void onClick(View v) {
-                                camera.takePicture(null, null, new PictureCallback() {
-                                        @Override
-                                        public void onPictureTaken(byte[] data, Camera camera) {
-                                                createPopup(data);
-                                                camera.startPreview();
-                                        }
-                                });
-                        }
-                });
-	    
-	   
+	    // photo button
+        findViewById(R.id.photo).setOnClickListener(new OnClickListener() {
+        	 @Override
+	            public void onClick(View v) {
+					Log.i("Testes", "Click Foto");
+					boolean saveButtonEnabled = false;
+					
+					// TO DO:
+					
+					speciesGuarda.setClickable(saveButtonEnabled);
+                }
+        });
+	
+        // save button
+        speciesGuarda.setOnClickListener(new OnClickListener() {
+        	@Override
+            public void onClick(View v) {
+				Log.i("Testes", "Click Guardar");
+				Log.i("Testes", speciesName.getText().toString());
+        	}
+        });
+        
+        speciesName.setOnKeyListener(new View.OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if(event.getAction() == KeyEvent.ACTION_UP)
+				{
+					Log.i("Testes", "Key pressed");
+				}
+				return false;
+			}
+        });
+        
+        
+        speciesGuarda.setClickable(false);
 	}
 	
 
@@ -115,5 +116,38 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+
+	@Override
+	public void onLocationChanged(Location location) 
+	{
+		Log.i("Testes", "Location changed");
+    	if(location != null)
+    	{
+    		lastLocation = location;
+    		updateUI();
+    	}
+	}
+
+
+	@Override
+	public void onProviderDisabled(String provider) 
+	{
+		// TODO Auto-generated method stub	
+	}
+
+
+	@Override
+	public void onProviderEnabled(String provider) 
+	{
+		// TODO Auto-generated method stub
+	}
+
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) 
+	{
+		// TODO Auto-generated method stub
 	}	
 }
